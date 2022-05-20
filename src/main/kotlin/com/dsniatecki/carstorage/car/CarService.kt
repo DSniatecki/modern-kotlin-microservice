18a8edc06cce18b095a2e35c5674ba2d8320f680
DSniatecki <damiansniatecki@gmail.com>
2022-05-20 15:14:43 +0200

    Fix CarService

--- src/main/kotlin/com/dsniatecki/carstorage/car/CarService.kt
+++ src/main/kotlin/com/dsniatecki/carstorage/car/CarService.kt
@@ -17,17 +17,17 @@ class CarService(
     fun getAll(): Flux<Car> =
         carRepository.findAll().sort(Comparator.comparing { it.createdAt })
 
-    fun save(carData: CarData): Mono<Car> = carRepository.save(createNewObject(carData))
+    fun save(carData: CarData): Mono<Car> = carRepository.save(createNewCar(carData))
 
     fun update(carId: String, carData: CarData): Mono<Car> =
         carRepository.findById(carId)
-            .flatMap { carRepository.save(updateObject(it, carData)) }
+            .flatMap { carRepository.save(updateCar(it, carData)) }
 
     fun delete(objectId: String): Mono<Unit> =
         carRepository.findById(objectId)
             .flatMap { carRepository.delete(objectId, timeSupplier.now()).switchIfEmpty(Mono.just(Unit)) }
 
-    private fun createNewObject(carData: CarData): Car =
+    private fun createNewCar(carData: CarData): Car =
         Car(
             id = generateId(),
             brand = carData.brand,
@@ -37,7 +37,7 @@ class CarService(
             updatedAt = null
         )
 
-    private fun updateObject(car: Car, carData: CarData): Car =
+    private fun updateCar(car: Car, carData: CarData): Car =
         car.copy(
             brand = carData.brand,
             model = carData.model,
